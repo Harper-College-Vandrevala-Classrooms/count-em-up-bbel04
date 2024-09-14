f47e107d3181db1144cc85a1099c43e9e364691f
bbel04 <158131166+bbel04@users.noreply.github.com>
2024-09-14 14:41:25 -0500

    Implement GroceryCounter.java solution, tests
    Implemented solution for the GroceryCounter.java file, implemented a driver program. Added unit tests to TestGroceryCounter.java

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -1,5 +1,126 @@
 package com.csc;
 
+//import java.util.Arrays;
+
+//import java.util.ArrayList;
+
 public class GroceryCounter {
-  // Put your code here!
+
+  private int[] counter = new int[]{0, 0, 0, 0};
+  private int overflows = 0;
+
+  void incrementHundredths() { 
+    // Check if max counter value is reached (9999), then reset and increment overflow
+    if (counter[0] == 9 && counter[1] == 9 && counter[2] == 9 && counter[3] == 9) {
+        counter[0] = 0;
+        counter[1] = 0;
+        counter[2] = 0;
+        counter[3] = 0;
+        overflows++;  // increment the overflow counter
+    } 
+    // increment logic
+    else {
+        if (counter[3] < 9) { 
+            counter[3]++;
+        } else { 
+            counter[3] = 0;
+            incrementTenths();
+        }
+    }
+  }
+
+  void incrementTenths() {
+    if (counter[2] < 9) {   //if number in tenths place is less than 9, increment
+      counter[2]++;
+    }
+    else {   //if number in tenths place is greater than or equal to 9, roll back to zero, increment ones
+      counter[2] = 0;
+      incrementOnes();
+    }
+  }
+
+  void incrementOnes() {    
+    if (counter[1] < 9) {   //if number in ones place is less than 9, increment 
+      counter[1]++;
+    }
+    else {    //if number in ones is greater than or equal to 9, roll back to zero, increment tens
+      counter[1] = 0;
+      incrementTens();
+    }
+  }
+
+  void incrementTens() {
+    if (counter[0] < 9) {  //if less than 9, increment
+      counter[0]++;
+    }
+    else {  //if greater than or equal to 9, roll back to zero, increment overflows
+      counter[0] = 0;
+      overflows++;
+    }
+  }
+
+  int numberOfOverflows() {
+    return overflows;
+  }
+
+  String total() {    //display in dollar format
+    if (counter[0] == 0) {
+      return String.format("$%d.%d%d", counter[1], counter[2], counter[3]);
+    }
+    else {
+      return String.format("$%d%d.%d%d", counter[0], counter[1], counter[2], counter[3]);
+    }
+  }
+
+  void clear() {    
+    for (int i = 0; i < counter.length; i++) {  //roll back all to zero
+      counter[i] = 0;
+    }
+    overflows = 0;   //reset overflows
+  }
+
+  public static void main(String[] args) {
+    GroceryCounter counter = new GroceryCounter();
+
+    //counter.incrementTenths();
+    for (int i = 0; i < 10000; i++) {
+      counter.incrementHundredths();
+    }
+    
+    System.out.println(counter.total()); // This would print out $00.00
+    System.out.println(counter.numberOfOverflows());
+    counter.clear();
+
+    for (int i = 0; i < 10; i++) {
+      counter.incrementOnes();
+    }
+
+    System.out.println(counter.total());
+
+    counter.incrementTens();
+    counter.incrementTens();
+    counter.incrementOnes();
+    counter.incrementHundredths();
+    counter.incrementTenths();
+
+    System.out.println(counter.total());
+
+    for (int i = 0; i < 30; i++) {
+      counter.incrementOnes();
+    }
+    
+    System.out.println(counter.total());
+
+    for (int i = 0; i < 22; i++) {
+      counter.incrementTens();
+    }
+
+    System.out.println(counter.total());
+    System.out.println(counter.numberOfOverflows());
+
+    counter.clear();   //roll back
+
+    System.out.println(counter.total());
+    System.out.println(counter.numberOfOverflows());
+  } 
 }
--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -14,11 +14,70 @@ public class TestGroceryCounter {
     counter = new GroceryCounter();
   }
 
-  // Put your tests here!
-
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void testIncrementHundredths() {
+    counter.incrementHundredths();
+    assertEquals(counter.total(), "$0.01");
   }
 
+  @Test
+  void testIncrementTenths() {
+    counter.incrementTenths();
+    assertEquals(counter.total(), "$0.10");
+  }
+
+  @Test
+  void testIncrementOnes() {
+    counter.incrementOnes();
+    assertEquals(counter.total(), "$1.00");
+  }
+
+  @Test
+  void testIncrementTens() {
+    counter.incrementTens();
+    assertEquals(counter.total(), "$10.00");
+  }
+
+  @Test
+  void testNumberOfOverflows() {
+    for (int i = 0; i < 11; i++) {
+      counter.incrementTens();
+    }
+    counter.numberOfOverflows();
+    assertEquals(counter.numberOfOverflows(), 1);
+  }
+
+  @Test
+  void testManyOverflows() {
+    for (int i = 0; i < 20; i++) {
+      counter.incrementTens();
+    }
+    counter.numberOfOverflows();
+    assertEquals(counter.numberOfOverflows(), 2);
+  }
+
+  @Test
+  void testCounterUpperLimit() {
+    for (int i = 0; i < 9999; i++) {
+      counter.incrementHundredths();
+    }
+    assertEquals(counter.total(), "$99.99");
+  }
+
+  @Test
+  void testOverflowAtUpperLimit() {
+    for (int i = 0; i < 10000; i++) {
+      counter.incrementHundredths();
+    }
+    assertEquals(counter.total(), "$0.00");
+  }
+
+  @Test
+  void testClear() {
+    for (int i = 0; i < 9; i++) {
+      counter.incrementTens();
+    }
+    counter.clear();
+    assertEquals(counter.total(), "$0.00");
+  }
 }
